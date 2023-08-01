d8848d5cd7db4762a24657dea8c7ff897006623f
Manato Yamashita <tcumanato@gmail.com>
2023-08-01 15:12:21 +0900

    Setting OGPs

--- src/views/Window5View.vue
+++ src/views/Window5View.vue
@@ -2,6 +2,14 @@
     <div class="w5" id="w5">
       <h2>スタート</h2>
       <h3>Comming Soon...</h3>
+      <p>このサイトはキラ大1周年を記念して制作されたWebサイトです。デザインは本研究会会長がデザインした「<a href="https://twitter.com/killerDie_/status/1647972629868085248?s=20">このTweet（x.com）を参考</a>」
+        に、本研究会員の匿名希望下マナトがコーディングしています。なお、当サイトはまだ未完成となっています。
+        内容はキラキラ大学生研究会で校閲した上で、徐々に皆さんと作っていきたいと考えています。
+    </p>
+    <p>
+        2023/07/29 : 匿名希望下
+    </p>
+
       <img src="@/assets/5.jpeg" alt="window" class="window">
       <img src="@/assets/logo.svg" alt="logo" class="logo" style="width: 30%;">
     </div>
